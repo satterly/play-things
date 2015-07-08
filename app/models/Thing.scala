98ca3c987c12a4a50cdc4986d74d5c1be65c7c54
Nick Satterly <nick.satterly@guardian.co.uk>
2015-07-08 19:22:01 +0100

    fix model

--- app/models/Thing.scala
+++ app/models/Thing.scala
@@ -4,7 +4,7 @@ import org.joda.time.DateTime
 
 case class Thing (
   id: Option[Long] = None,
-                 title: String,
+  title: String,
   note: String,
   createdAt: DateTime = new DateTime(),
   lastModified: DateTime = new DateTime(),
@@ -12,15 +12,15 @@ case class Thing (
 
 object Thing {
 
+  def search(q: String): Seq[Thing] = ???
+
   def all: List[Thing] = ???
 
   def findById(id: Long): Option[Thing] = ???
 
-  def search(q: String): Seq[Thing] = ???
+  def create(name: String, note: String, status: String): Option[Thing] = ???
 
   def update(id: Long, thing: Thing): Boolean = ???
-
-  def create(name: String, note: String, status: String): Option[Thing] = ???
 
   def delete(id: Long): Boolean = ???
 }
